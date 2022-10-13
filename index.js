cd78afcd6762e294a2d7a7c838370027dce7c78a
cberescu <ciprian@berescu.com>
2022-10-13 15:44:58 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const resolve = require('path').resolve;
+const ETA = require('eta')
 const fastify = require('fastify')({
   logger:true,
   ignoreTrailingSlash: true,
@@ -21,13 +22,21 @@ const fastify = require('fastify')({
 
 fastify.get('/',async (request,reply)=>{
 	try {
-		return await fastify.view('view.eta',{data:false});
+		return await ETA.renderFile('web/view.eta',{data:false});
 	} catch (e) {
 		return 'Error found'
 	}
 
 })
+fastify.get('/a',async (request,reply)=>{
+	try {
+		return await ETA.renderFile(path.join(__dirname, 'web','view.eta'),{data:false},ETA.getConfig({ async: true }));
+	} catch (e) {
+    console.log(e);
+		return 'Error found'
+	}
 
+})
 fastify.listen({port: process.env.PORT || 80, host:"0.0.0.0"} , (err, address) => {
 
   if (err) throw err
